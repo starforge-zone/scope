dbd9f7c325fdfeb8c50abd2e59a7f52232082026
kongiok <ong.kongiok@gmail.com>
2025-08-29 19:56:25 +0800

    feat(layout): add licensing publishing our contents

--- app/components/organisms/footer.vue
+++ app/components/organisms/footer.vue
@@ -3,7 +3,34 @@
     class="p-2 min-h-72 w-full border-2 flex flex-col justify-center-safe content-center"
   >
     <o-cta-email-sub class="flex-1" />
-    <!-- Copyright and License -->
-    <section class="flex-1"></section>
+    <!-- Logo and Licenses -->
+    <section
+      class="flex-1 my-3 px-5 flex flex-col-reverse md:flex-row justify-center-safe md:justify-around items-center-safe"
+    >
+      <div class="flex-auto w-fit">
+        <u-link to="/about/us">
+          <span
+            class="text-primary-600 font-extrabold size-5 hover:drop-shadow active:drop-shadow-none"
+            >Starforge</span
+          >
+        </u-link>
+      </div>
+      <!-- License -->
+      <div class="flex-auto w-fit text-center text-base text-neutral-600">
+        <p>
+          make with
+          <u-link to="/about/us">
+            <u-icon
+              name="hugeicons:love-korean-finger"
+              class="font-bold size-5"
+            />
+          </u-link>
+          and
+          <u-link to="/about/licenses" class="font-bold active:text-primary-500"
+            >FOSS</u-link
+          >
+        </p>
+      </div>
+    </section>
   </footer>
 </template>
